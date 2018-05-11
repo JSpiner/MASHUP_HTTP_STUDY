6c02634a800fad32378cec9db2f5681de6ea9c4e
JSpiner <jspiner@naver.com>
2018-05-11 12:06:52 +0900

    스플래시 페이지 레이아웃 구성

--- app/src/main/java/kr/mash_up/SplashActivity.java
+++ app/src/main/java/kr/mash_up/SplashActivity.java
@@ -1,10 +1,18 @@
 package kr.mash_up;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -20,7 +28,28 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void init() {
+        initStatusBar();
         startMainActivityWithDelay();
+    }
+
+    private void initStatusBar() {
+        setWindowLightStatusBarEnable();
+        setStatusBarColor(Color.WHITE);
+    }
+
+    protected void setStatusBarColor(@ColorInt int color) {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(color);
+    }
+
+    protected void setWindowLightStatusBarEnable() {
+        View view = findViewById(R.id.root);
+
+        int flags = view.getSystemUiVisibility();
+        flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        view.setSystemUiVisibility(flags);
     }
 
     private void startMainActivityWithDelay() {
